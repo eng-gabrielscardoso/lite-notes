68de885dd562fb9cb5295777d0e532eea9ccac68
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-04-09 01:43:12 -0300

    feat: migration to add uuids in notes table

---
+++ database/migrations/2023_04_09_043416_add_uuid_on_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->uuid('public_id')->after('id');
+        });
+
+        DB::table('notes')->whereNull('public_id')->chunkById(200, function ($notes) {
+            foreach ($notes as $note) {
+                DB::table('notes')->where('id', $note->id)->update(['public_id' => Uuid::uuid4()->toString()]);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->dropColumn('public_id');
+        });
+    }
+};
